699c6b23368058b535b590429ef31812b53ef212
DQThang499 <quocthang12a5@gmail.com>
2021-08-05 15:41:18 +0700

    add file about.java

---
+++ src/baitap/bt9/about.java
@@ -0,0 +1,16 @@
+package baitap.bt9;
+
+public class about {
+    protected String hoten;
+    public about (String hoten){
+        this.hoten = hoten;
+    }
+
+    public String getHoten() {
+        return hoten;
+    }
+
+    public void setHoten(String hoten) {
+        this.hoten = hoten;
+    }
+}
