04f89e06ad94b6e523cdc914b6eca8945cc78729
DQThang499 <88323631+DQThang499@users.noreply.github.com>
2021-08-05 15:44:57 +0700

    Update about.java

--- src/baitap/bt9/about.java
+++ src/baitap/bt9/about.java
@@ -2,8 +2,10 @@ package baitap.bt9;
 
 public class about {
     protected String hoten;
-    public about (String hoten){
+    protected String diachi;
+    public about (String hoten, String diachi){
         this.hoten = hoten;
+        this.diachi = diachi;
     }
 
     public String getHoten() {
@@ -12,5 +14,13 @@ public class about {
 
     public void setHoten(String hoten) {
         this.hoten = hoten;
+    }
+    
+    public String getDiachi() {
+        return diachi;
+    }
+
+    public void setDiachi(String diachi) {
+        this.diachi = diachi;
     }
 }
